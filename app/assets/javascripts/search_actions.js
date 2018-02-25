ffd51d1f5ced0f491a76905b446b6e76c5111974
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-02-25 02:29:59 -0800

    Creating tabs and table data

--- app/assets/javascripts/search_actions.js
+++ app/assets/javascripts/search_actions.js
@@ -1,12 +1,11 @@
 $(document).ready(function(e){
-    $('#menu_types').click(function(e) {
-    	// alert("Does this work ?");
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
 		e.preventDefault();
 		var param = $(this).attr("href").replace("#","");
 		var concept = $(this).text();
 		$('.search-panel span#search_concept').text(concept);
 		$('.input-group #search_param').val(param);
-	});	
+	});
 
 	$('#search_button').click(function(e) {
 		e.preventDefault();
@@ -20,7 +19,6 @@ $(document).ready(function(e){
 		    success: function(data){
 		    	// alert("Success !! \n" + data);
 		    	$('#search_results_container').html(data);
-		    	// $('#search_results_container').html("<%= render :partial => 'search_results', :locals => { :search_results => data} %>");
 		    },    	
 		    error: function(error){
 		    	alert("Search not successful ! \n" + error.responseText);
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,23 +3,27 @@ class SearchController < ApplicationController
   end
 
   def search_results
+    @filtered_tickets, filtered_users, @filtered_orgs = nil
+    @no_results = false
   	search_text = params[:search_text]
   	search_param = params[:search_param]
   	print params
   	@tickets = JSON.parse(File.read('public/tickets.json'))
   	@users = JSON.parse(File.read('public/users.json'))
   	@orgs = JSON.parse(File.read('public/organizations.json'))
-  	# print "\nThis prints all tickets: \n"
-    # @filtered_tickets = @tickets.map {|key, value| print value if value.include?(search_text)}
-    @filtered_tickets = @tickets.select {|ticket| ticket.to_s.downcase.include?(search_text.downcase)}
-    @filtered_users = @users.select {|user| user.to_s.downcase.include?(search_text.downcase)}
-    @filtered_orgs = @orgs.select {|org| org.to_s.downcase.include?(search_text.downcase)}
- 	# respond_to do |format|
- 		 # format.js
-       # format.js  { render :partial => "search_results", :formats => [:html, :js, :json, :url_encoded_form], :locals => { :search_results => @tickets } }
-       # format.json  { render :json => { :search_results => @tickets } }
-     # end
+    if search_param.eql?("all" || "tickets") || search_param.eql?("tickets")
+      @filtered_tickets = @tickets.select {|ticket| ticket.to_s.downcase.include?(search_text.downcase)} 
+    end
+    if search_param.eql?("all") || search_param.eql?("users")
+      @filtered_users = @users.select {|user| user.to_s.downcase.include?(search_text.downcase)}
+    end
+    if search_param.eql?("all") || search_param.eql?("organizations")
+      @filtered_orgs = @orgs.select {|org| org.to_s.downcase.include?(search_text.downcase)}
+    end
+    @no_results = (@filtered_tickets || @filtered_users || @filtered_orgs).empty? ? true : false
     render :partial => "search_results", :locals => { :ticket_results => @filtered_tickets, 
-    	:user_results => @filtered_users, :org_results => @filtered_orgs  }
+    	                                   :user_results => @filtered_users, 
+                                         :org_results => @filtered_orgs,
+                                         :no_results => @no_results }
   end
 end
