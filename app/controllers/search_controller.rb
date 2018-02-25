152953b904a5c6b40adf20438e6e6dfea5cca138
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-02-25 03:09:25 -0800

    Null text search
    Searches results for an empty string

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,20 +5,32 @@ class SearchController < ApplicationController
   def search_results
     @filtered_tickets, filtered_users, @filtered_orgs = nil
     @no_results = false
-  	search_text = params[:search_text]
+  	search_text = params[:search_text].strip
   	search_param = params[:search_param]
   	print params
   	@tickets = JSON.parse(File.read('public/tickets.json'))
   	@users = JSON.parse(File.read('public/users.json'))
   	@orgs = JSON.parse(File.read('public/organizations.json'))
     if search_param.eql?("all" || "tickets") || search_param.eql?("tickets")
-      @filtered_tickets = @tickets.select {|ticket| ticket.to_s.downcase.include?(search_text.downcase)} 
+      if search_text.eql?("") 
+        @filtered_tickets = @tickets.select {|ticket| ticket.has_value?("")} 
+      else
+        @filtered_tickets = @tickets.select {|ticket| ticket.to_s.downcase.include?(search_text.downcase)} 
+      end
     end
     if search_param.eql?("all") || search_param.eql?("users")
-      @filtered_users = @users.select {|user| user.to_s.downcase.include?(search_text.downcase)}
+      if search_text.eql?("") 
+        @filtered_users = @users.select {|user| user.has_value?("")} 
+      else
+        @filtered_users = @users.select {|user| user.to_s.downcase.include?(search_text.downcase)}
+      end
     end
     if search_param.eql?("all") || search_param.eql?("organizations")
-      @filtered_orgs = @orgs.select {|org| org.to_s.downcase.include?(search_text.downcase)}
+      if search_text.eql?("") 
+        @filtered_orgs = @orgs.select {|org| org.has_value?("")} 
+      else
+        @filtered_orgs = @orgs.select {|org| org.to_s.downcase.include?(search_text.downcase)}
+      end
     end
     @no_results = (@filtered_tickets || @filtered_users || @filtered_orgs).empty? ? true : false
     render :partial => "search_results", :locals => { :ticket_results => @filtered_tickets, 
