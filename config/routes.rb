1923ef13af3c21b9f0306b566c44afe2ccc86ac8
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-02-26 14:55:45 -0800

    Removed commented lines

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get "search/index"
-  # get "search/search_results"
 
   get  '/search_results', to: 'search#search_results'
 
-  # get "search/search_results"
-
-  # resource :search do
-  # 	get "index"
-  # 	get "search_results"
-  # end
-
-  # map.root :controller => 'pages', :action => 'home'
   root "search#index"
+  
 end
